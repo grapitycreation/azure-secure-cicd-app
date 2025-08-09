840bd1e2e5c38ed162b99fd1c507f00fbeb60a50
grapitycreation <tranhieu123letgo@gmail.com>
2025-08-09 15:28:59 +0700

    Initial commit: Create simple Flask application

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    # Lấy một biến môi trường để thử nghiệm, nếu không có thì dùng giá trị mặc định
+    greeting = os.environ.get("GREETING", "Hello")
+    return f"<h1>{greeting}, World from Azure App Service!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
