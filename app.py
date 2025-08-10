bbac783c12770ae9a7e42f7cb2126cbea41b812a
grapitycreation <tranhieu123letgo@gmail.com>
2025-08-10 20:27:17 +0700

    Test CI/CD

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     # Lấy một biến môi trường để thử nghiệm, nếu không có thì dùng giá trị mặc định
     greeting = os.environ.get("GREETING", "Hello")
-    return f"<h1>{greeting}, This is a test!</h1>"
+    return f"<h1>{greeting}, World from Azure App Service!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
